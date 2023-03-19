2cb3e9dcab110dea219d0310158dd64a7e7d34cb
muhammadasadullahtariq <asadullahtariq8995@gmail.com>
2023-03-19 22:26:30 +0500

    app completed and functional

--- ios/FilmXTests/FilmXTests.m
+++ ios/FilmXTests/FilmXTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface pocketMoneyTests : XCTestCase
+@interface FilmXTests : XCTestCase
 
 @end
 
-@implementation pocketMoneyTests
+@implementation FilmXTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
